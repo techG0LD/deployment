131154817f1825b72fccc19673eb4756ec51659a
mathew puig <puigster956@gmail.com>
2023-07-16 12:28:26 -0700

    launch

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const PORT = process.env.PORT
+
+
+app.get('/',(req,res) => {
+    res.send('hello world!')
+})
+
+app.listen(PORT , () => {
+    console.log('http://localhost:3000')
+})
+
+module.exports = app
